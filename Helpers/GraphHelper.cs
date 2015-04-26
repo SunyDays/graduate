281d7f40ca4e21fb716f5e70740c2d5aa8ba3516
SunyDays <sunnyddayss@gmail.com>
2015-04-26 15:30:01 +0300

    method for finding all paths in graph between two nodes

---
+++ Helpers/GraphHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Types;
+using System.Linq;
+
+namespace Helpers
+{
+    public static class GraphHelper
+    {
+        public static List<Vector<int>> GetAllPaths(Matrix<double> matrix, int startNode, int targetNode)
+        {
+            var paths = new List<Vector<int>>();
+
+            // exclude routing loops
+            matrix.ForEachRow(row => row[startNode] = 0);
+
+            for (int intermediateNode = 0; intermediateNode < matrix.RowsCount; intermediateNode++)
+            {
+                if (matrix[startNode, intermediateNode] <= 0)
+                    continue;
+
+                if (intermediateNode == targetNode)
+                    paths.Add(new Vector<int>(new[] { startNode, targetNode }));
+                else
+                {
+                    List<Vector<int>> localPaths = GetAllPaths(matrix.Clone(), intermediateNode, targetNode);
+                    if (localPaths.Any())
+                        localPaths.ForEach(path => paths.Add(path.InsertElement(0, startNode)));
+                }
+            }
+
+            return paths;
+        }
+    }
+}
