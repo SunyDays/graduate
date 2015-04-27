a67329499adbcefff01b4fd87123f18719676c23
SunyDays <sunnyddayss@gmail.com>
2015-04-27 03:20:05 +0300

    * GraduationProject/Main.csproj:
    * GraduationProject/Program.cs:
      console output for integrated PTC

--- GraduationProject/Program.cs
+++ GraduationProject/Program.cs
@@ -15,33 +15,8 @@ namespace Main
             if (args.Length != 1)
                 throw new ArgumentException("Need config file", "args");
 
-            var networkModel = new NetworkModel(args[0]);
+            var networkModel = new NetworkModel(args[0], 0, 3);
             ConsoleOutput(networkModel);
-
-//            TestGuassMethod();
-
-//            var A = new Matrix<double>(new double[,]
-//                {
-//                    { -1, 0, 0.1, 0, 0 },
-//                    { 0, -1, 0, 0, 0 },
-//                    { 0, 0.2, -1, 0.2, 0 },
-//                    { 1, 0.2, 0, -1, 0 },
-//                    { 0, 0.6, 0, 0, -1 }
-//                });
-//            var b = new Vector<double>(new double[]{ -0.3333, -0.6667, 0, 0, 0 });
-
-//            var A = new Matrix<double>(new double[,]
-//                {
-//                    { 2, 1, -1},
-//                    { -3, -1, 2 },
-//                    { -2, 1, 2 }
-//                });
-//            var b = new Vector<double>(new double[]{ 8, -11, -3 });
-//
-//            var result = GaussMethod.Solve(A, b);
-//            result.GetList().ForEach(Console.WriteLine);
-
-//            var dv = new DoubleVector();
 
             Console.ReadLine();
         }
@@ -77,24 +52,9 @@ namespace Main
                         networkModel.Lambda[stream][i], networkModel.Mu[stream][i], networkModel.Ro[stream][i]);
                 Console.WriteLine();
 
-//                ColorWriteLine("Lambda", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Lambda[stream].Length; i++)
-//                    Console.WriteLine("{0}\t", networkModel.Lambda[stream][i]);
-//                Console.WriteLine ();
-
                 ColorWriteLine("Lambda0", ConsoleColor.Green);
                 Console.WriteLine(networkModel.Lambda0[stream]);
                 Console.WriteLine ();
-
-//                ColorWriteLine("Mu", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Mu[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.Mu[stream][i]);
-//                Console.WriteLine ();
-//
-//                ColorWriteLine("Ro", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Ro[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.Ro[stream][i]);
-//                Console.WriteLine();
 
                 ColorWriteLine("E", ConsoleColor.Green);
                 for (int i = 0; i < networkModel.E[stream].Length; i++)
@@ -107,22 +67,12 @@ namespace Main
                         networkModel.LambdaBar[stream][i], networkModel.RoBar[stream][i]);
                 Console.WriteLine();
 
-//                ColorWriteLine("LambdaBar", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.LambdaBar[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.LambdaBar[stream][i]);
-//                Console.WriteLine();
-//
-//                ColorWriteLine("RoBar", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.RoBar[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.RoBar[stream][i]);
-//                Console.WriteLine();
-
                 ColorWriteLine("RoTotal", ConsoleColor.Green);
                 for (int i = 0; i < networkModel.RoTotal.Length; i++)
                     Console.WriteLine("{0:0.000}\t", networkModel.RoTotal[i]);
                 Console.WriteLine();
 
-                ColorWriteLine("Stationary probability-time characteristic", ConsoleColor.Green);
+                ColorWriteLine("Stationary probability-time characteristics", ConsoleColor.Green);
                 ColorWriteLine("Ws", ConsoleColor.Green);
                 for (int i = 0; i < networkModel.Ws.Length; i++)
                     Console.WriteLine("{0:0.000}\t", networkModel.Ws[i]);
@@ -134,20 +84,15 @@ namespace Main
                         networkModel.Us[stream][i], networkModel.Ls[stream][i], networkModel.Ns[stream][i]);
                 Console.WriteLine();
 
-//                ColorWriteLine("Us", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Us[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.Us[stream][i]);
-//                Console.WriteLine();
-//
-//                ColorWriteLine("Ls", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Ls[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.Ls[stream][i]);
-//                Console.WriteLine();
-//
-//                ColorWriteLine("Ns", ConsoleColor.Green);
-//                for (int i = 0; i < networkModel.Ns[stream].Length; i++)
-//                    Console.WriteLine("{0:0.000}\t", networkModel.Ns[stream][i]);
-//                Console.WriteLine();
+                ColorWriteLine("Integrated probability-time characteristics", ConsoleColor.Green);
+                ColorWriteLine("Wi", ConsoleColor.Green);
+                Console.WriteLine(networkModel.Wi);
+                Console.WriteLine();
+
+                ColorWriteLine("Ui\t\tLi\t\tNi", ConsoleColor.Green);
+                    Console.WriteLine("{0:0.000}\t\t{1:0.000}\t\t{2:0.000}",
+                        networkModel.Ui[stream], networkModel.Li[stream], networkModel.Ni[stream]);
+                Console.WriteLine();
 
                 ColorWriteLine("==================================================", ConsoleColor.White);
             }
@@ -163,36 +108,22 @@ namespace Main
             Console.ForegroundColor = originalColor;
         }
 
-        private static void TestProtocolHelper()
-        {
-            foreach (ulong ethernetType in Enum.GetValues(typeof(ProtocolHelper.EthernetType)))
-                foreach (int bound in Enum.GetValues(typeof(ProtocolHelper.FrameRange)))
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Protocol = {0} Ethernet", (ProtocolHelper.EthernetType)ethernetType);
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    Console.WriteLine("bitRate = {0} bit/s\nframeLength = {1} bytes\ncapacity = {2} frames/milisecond",
-                        ProtocolHelper.GetBitRate((ProtocolHelper.EthernetType)ethernetType),
-                        bound,
-                        ProtocolHelper.GetCapacity((ProtocolHelper.EthernetType)ethernetType, bound));
-
-                    Console.WriteLine();
-                }
-        }
-
-        private static void TestGuassMethod()
-        {
-            var A = new double[,]
-            {
-                {-1, 0, 0.1, 0, 0},
-                {0, -1, 0, 0, 0},
-                {0, 0.2, -1, 0.2, 0},
-                {1, 0.2, 0, -1, 0},
-                {0, 0.6, 0, 0, -1}
-            };
-            var b = new double[]{ -0.3333, -0.6667, 0, 0, 0};
-//            GaussMethod.Solve(A, b);
-        }
+//        private static void TestProtocolHelper()
+//        {
+//            foreach (ulong ethernetType in Enum.GetValues(typeof(ProtocolHelper.EthernetType)))
+//                foreach (int bound in Enum.GetValues(typeof(ProtocolHelper.FrameRange)))
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Green;
+//                    Console.WriteLine("Protocol = {0} Ethernet", (ProtocolHelper.EthernetType)ethernetType);
+//                    Console.ForegroundColor = ConsoleColor.White;
+//
+//                    Console.WriteLine("bitRate = {0} bit/s\nframeLength = {1} bytes\ncapacity = {2} frames/milisecond",
+//                        ProtocolHelper.GetBitRate((ProtocolHelper.EthernetType)ethernetType),
+//                        bound,
+//                        ProtocolHelper.GetCapacity((ProtocolHelper.EthernetType)ethernetType, bound));
+//
+//                    Console.WriteLine();
+//                }
+//        }
     }
 }
